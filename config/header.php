ec92e41900fe3af4fb29da227aa7762f8909a5d7
feddericahu99 <federica.hu339@gmail.com>
2022-09-15 21:38:20 +0200

    header fixed

---
+++ config/header.php
@@ -0,0 +1,44 @@
+<?php 
+return [
+    [
+        'name' => 'characters',
+        'url' => '/characters'
+    ],
+    [
+        'name' => 'comics',
+        'url' => '/comics'
+    ],
+    [
+        'name' => 'movies',
+        'url' => '/movies'
+    ],
+    [
+        'name' => 'tv',
+        'url' => '/tv'
+    ],
+    [
+        'name' => 'games',
+        'url' => '/games'
+    ],
+    [
+        'name' => 'collectibles',
+        'url' => '/collectibles'
+    ],
+    [
+        'name' => 'videos',
+        'url' => '/videos'
+    ],
+    [
+        'name' => 'fans',
+        'url' => '/fans'
+    ],
+    [
+        'name' => 'news',
+        'url' => '/news'
+    ],
+    [
+        'name' => 'shop',
+        'url' => '/shop'
+    ],
+
+    ];
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,32 @@
+<?php
+$links = config('header');
+
+?>
+
 <header>
-    <p>sono l'header</p>
+<!-- upper header -->
+    <div id="upper-header">
+        <ul id="flex-upper-header" class="container">
+           <li><a href="">DC POWER VISA</a></li>
+           <li><a href="">ADDITIONAL DC SITES</a></li>
+        </ul>
+    </div>
+<!-- bottom header -->
+    <div id="bottom-header">
+        <div class="container flex-bottom">
+            <figure>
+                <a href="">
+                    <img src=" {{ asset('img/dc-logo.png') }} " alt="">
+                </a>
+            </figure>
+            <ul class="flex-bottom">
+                @foreach($links as $link)
+                <li><a href="">{{$link['name']}}</a></li>
+                @endforeach
+            </ul>
+            <div> 
+                <input type="text" placeholder="Search.." id="header-search">
+            </div>
+        </div>
+    </div>
 </header>
