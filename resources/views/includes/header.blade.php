2f3783dec24853e26da7b047209fbce760c15dcb
feddericahu99 <federica.hu339@gmail.com>
2022-09-15 16:28:07 +0200

    structure

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <p>sono l'header</p>
+</header>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,15 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
+        <link rel="stylesheet" href="css/style.css">
+        <title>DC COMICS</title>
     </head>
     <body>
-        <header>we</header>
+
+    <!-- header -->
+    @include('includes.header')
+    <!-- main -->
+    @yield('content')
+    <!-- footer -->
+    @include('includes.footer')
     </body>
 </html>
