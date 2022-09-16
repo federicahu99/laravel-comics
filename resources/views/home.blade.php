f6d76f2817e33c707e623b83907bf21bfc57b209
feddericahu99 <federica.hu339@gmail.com>
2022-09-16 15:51:29 +0200

    added comics in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,18 @@
+<?php
+$comics = config('comics');
+?>
+
 @extends('layouts.main')
 
 @section('content')
-<h1>Home Page</h1>
+
+    @foreach($comics as $comic)
+        <ul>
+            <li>
+                <img src="{{ $comic['thumb'] }}" alt=" {{ $comic['series']}} ">
+                <h4>{{ $comic['series']}}</h4>
+            </li>
+        </ul>
+    @endforeach
+
 @endsection
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>shop not found</h1>
+<h1>Shop content not found</h1>
 @endsection
